85928e95a3da04d660659e3b4b962d78a87dfdcc
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-08 20:01:38 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ num2 = list(map(int, input().split()))
 # Make your Code
 # ******************************
 
+counter = 0
+for i in range(len(num2)):
+    for j in range(len(num1)):
+        if(num2[i] == num1[j]):
+            counter += 1
+            break
+if counter == len(num2):
+    print('True')
+else:
+    print('False')
 # print ('True') or print ('False')
